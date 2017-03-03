4d1ef02b908697758fb85b0ee31afb845602e512
Paul Tevis <paul.tevis@appfolio.com>
2017-03-02 16:33:04 -0800

    Initial flow

--- app/controllers/fizz_buzz_controller.rb
+++ app/controllers/fizz_buzz_controller.rb
@@ -1,2 +1,8 @@
 class FizzBuzzController < ApplicationController
+  def new
+  end
+
+  def create
+    params.require(:fizz_buzz).permit(:number)
+  end
 end
--- test/controllers/fizz_buzz_controller_test.rb
+++ test/controllers/fizz_buzz_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class FizzBuzzControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'can get new page' do
+    get new_fizz_buzz_path
+    assert_response :ok
+    assert_select "form[action='#{fizz_buzz_index_url}'][method=post]" do
+      assert_select 'input[name="fizz_buzz[number]"]'
+      assert_select 'input[type=submit]'
+    end
+  end
+
+  test 'can post to create page' do
+    post fizz_buzz_index_path, xhr: true, params: { fizz_buzz: { number: '0' } }
+    assert_response :ok
+  end
 end
