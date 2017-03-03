2682003845de161207bd07a62cd3ced6d41cf4bf
Paul Tevis <paul.tevis@appfolio.com>
2017-03-03 12:11:04 -0800

    Fix up tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :fizz_buzz, only: [:new, :create]
+
+  root to: 'fizz_buzz#new'
 end
--- test/controllers/fizz_buzz_controller_test.rb
+++ test/controllers/fizz_buzz_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class FizzBuzzControllerTest < ActionDispatch::IntegrationTest
-  test 'can get new page' do
+  test 'new page contains form to input number to create' do
     get new_fizz_buzz_path
     assert_response :ok
     assert_select "form[action='#{fizz_buzz_index_url}'][method=post]" do
@@ -10,8 +10,10 @@ class FizzBuzzControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test 'can post to create page' do
+  test 'create page contains answer and link back to new page' do
     post fizz_buzz_index_path, xhr: true, params: { fizz_buzz: { number: '0' } }
     assert_response :ok
+    assert_select '#fizz_buzz_answer', text: '0'
+    assert_select "a[href='#{new_fizz_buzz_url}']"
   end
 end
