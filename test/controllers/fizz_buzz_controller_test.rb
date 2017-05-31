e48ee39d45b497d0fa71cc5bdf354fe6a3ece8c1
Paul Tevis <paul.tevis@appfolio.com>
2017-05-31 12:52:55 -0700

    Change controller test to use ActionController::TestCase

--- test/controllers/fizz_buzz_controller_test.rb
+++ test/controllers/fizz_buzz_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class FizzBuzzControllerTest < ActionDispatch::IntegrationTest
+class FizzBuzzControllerTest < ActionController::TestCase
   test 'new page contains form to input number to create' do
-    get new_fizz_buzz_path
+    get :new
     assert_response :ok
     assert_select "form[action='#{fizz_buzz_index_url}'][method=post]" do
       assert_select 'input[name="fizz_buzz[number]"]'
@@ -10,14 +10,15 @@ class FizzBuzzControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test 'create page contains link back to new page' do
-    post fizz_buzz_index_path, xhr: true, params: { fizz_buzz: { number: '0' } }
+  test 'create page contains answer and link back to new page' do
+    post :create, params: { fizz_buzz: { number: '0' } }
     assert_response :ok
-    assert_select "a[href='#{new_fizz_buzz_url}']"
+    assert_select '.js-fizz_buzz_answer'
+    assert_select "a[href='#{new_fizz_buzz_url}']", text: 'FizzBuzz again!'
   end
 
   test 'create with 1 displays 1' do
-    post fizz_buzz_index_path, xhr: true, params: { fizz_buzz: { number: '1' } }
+    post :create, params: { fizz_buzz: { number: '1' } }
     assert_response :ok
     assert_select '.js-fizz_buzz_answer', text: '1'
   end
