9a4093b3de78285b08a27fe4c6716fa1b56c18e1
Paul Tevis <paul.tevis@appfolio.com>
2017-05-31 12:40:30 -0700

    Clean up test cases

--- app/controllers/fizz_buzz_controller.rb
+++ app/controllers/fizz_buzz_controller.rb
@@ -1,15 +1,13 @@
-class FizzBuzzer
-  def self.fizz_buzz(_num)
-    0
-  end
-end
-
 class FizzBuzzController < ApplicationController
   def new
   end
 
   def create
     number = params.require(:fizz_buzz).permit(:number)[:number]
-    render locals: { answer: FizzBuzzer.fizz_buzz(number) }
+    render locals: { answer: FizzBuzzController.fizz_buzz(number) }
+  end
+
+  def self.fizz_buzz(_num)
+    1
   end
 end
--- test/controllers/fizz_buzz_controller_test.rb
+++ test/controllers/fizz_buzz_controller_test.rb
@@ -1,11 +1,5 @@
 require 'test_helper'
 
-class FizzBuzzerTest < ActiveSupport::TestCase
-  test 'foo' do
-    assert_equal FizzBuzzer.fizz_buzz(0), 0
-  end
-end
-
 class FizzBuzzControllerTest < ActionDispatch::IntegrationTest
   test 'new page contains form to input number to create' do
     get new_fizz_buzz_path
@@ -16,10 +10,19 @@ class FizzBuzzControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test 'create page contains answer and link back to new page' do
+  test 'create page contains link back to new page' do
     post fizz_buzz_index_path, xhr: true, params: { fizz_buzz: { number: '0' } }
     assert_response :ok
-    assert_select '.js-fizz_buzz_answer', text: '0'
     assert_select "a[href='#{new_fizz_buzz_url}']"
+  end
+
+  test 'create with 1 displays 1' do
+    post fizz_buzz_index_path, xhr: true, params: { fizz_buzz: { number: '1' } }
+    assert_response :ok
+    assert_select '.js-fizz_buzz_answer', text: '1'
+  end
+
+  test 'fizz_buzz with 1 returns 1' do
+    assert_equal 1, FizzBuzzController.fizz_buzz(1)
   end
 end
