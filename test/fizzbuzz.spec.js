a5e02e27d34b852ffc86c4e94fba127d39e2c038
Timo Van-Kissing <timo.van-kissing@appfolio.com>
2021-04-01 14:05:12 -0700

    test: add first fizzbuzz spec

--- test/fizzbuzz.spec.js
+++ test/fizzbuzz.spec.js
@@ -1,17 +1,30 @@
-import sinon from 'sinon';
+import { expect } from "chai";
 
 import { fizzbuzz } from "../src/fizzbuzz";
 
 describe('fizzbuzz', () => {
 
-  let logStub;
+  it('fails if passed a bad argument', () => {
+    expect(() => fizzbuzz(-1)).to.throw();
+    expect(() => fizzbuzz('foo')).to.throw();
+    expect(() => fizzbuzz(14.1)).to.throw();
+    expect(() => fizzbuzz(15)).to.throw();
+  });
 
-  beforeEach(() => {
-    logStub = sinon.stub(console, 'log');
-  })
+  it('returns Fizz if the integer is evenly divisible by 3', () => {
+    expect(fizzbuzz(3)).to.equal('Fizz');
+    expect(fizzbuzz(9)).to.equal('Fizz');
+  });
 
-  it('prints numbers', () => {
-    fizzbuzz(1, 14);
-    sinon.assert.called(logStub);
-  })
+  it('returns Buzz if the integer is evenly divisible by 5', () => {
+    expect(fizzbuzz(5)).to.equal('Buzz');
+    expect(fizzbuzz(10)).to.equal('Buzz');
+  });
+
+  it('returns the integer otherwise', () => {
+    let input = 7;
+    expect(fizzbuzz(input)).to.equal(String(input));
+    input = 13;
+    expect(fizzbuzz(input)).to.equal(String(input));
+  });
 });
