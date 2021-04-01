40c112e30f70765d9a156133f8058624200ffefe
Timo Van-Kissing <timo.van-kissing@appfolio.com>
2021-04-01 14:06:35 -0700

    feat: implement fizzbuzz 1.0

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,3 +1,24 @@
-export function fizzbuzz(from, to) {
-  // TODO
+export function fizzbuzz(int) {
+  if (typeof int != 'number') {
+    throw Error('Bad argument');
+  }
+  if (!/^\d+$/.test(int)) {
+    throw Error('Bad argument');
+  }
+  if (int < 1) {
+    throw Error('Bad argument');
+  }
+  if (int > 14) {
+    throw Error('Bad argument');
+  }
+
+  if (int % 3 == 0) {
+    return 'Fizz';
+  }
+
+  if (int % 5 == 0) {
+    return 'Buzz';
+  }
+
+  return String(int);
 }
