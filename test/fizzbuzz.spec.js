a2d99e9e6d8fd90302840ea57e88b1eb0cc93368
Timo Van-Kissing <timo.van-kissing@appfolio.com>
2021-04-29 13:59:41 -0700

    test: first failing test

--- test/fizzbuzz.spec.js
+++ test/fizzbuzz.spec.js
@@ -1,6 +1,6 @@
 import sinon from 'sinon';
 
-import { fizzbuzz } from "../src/fizzbuzz";
+import { fizzbuzz } from '../src/fizzbuzz';
 
 describe('fizzbuzz', () => {
 
@@ -8,10 +8,29 @@ describe('fizzbuzz', () => {
 
   beforeEach(() => {
     logStub = sinon.stub(console, 'log');
+  });
+
+  afterEach(() => {
+    sinon.restore();
   })
 
-  it('prints numbers', () => {
-    fizzbuzz(1, 14);
-    sinon.assert.called(logStub);
-  })
+  it('prints Fizz if the input is 3', () => {
+    fizzbuzz(3);
+
+    sinon.assert.calledOnceWithExactly(logStub, 'Fizz');
+  });
+
+  it('prints Buzz if the input is 5', () => {
+    fizzbuzz(5);
+
+    sinon.assert.calledOnceWithExactly(logStub, 'Buzz');
+  });
+
+  it('prints 7 if the input is 7', () => {
+    fizzbuzz(7);
+
+    sinon.assert.calledOnceWithExactly(logStub, 7);
+  });
+
+
 });
