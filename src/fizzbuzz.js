3404218e5f2c58804369b4c1d71eb97687327f03
Timo Van-Kissing <timo.van-kissing@appfolio.com>
2021-04-29 13:59:53 -0700

    feat: implement first version

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,3 +1,10 @@
-export function fizzbuzz(from, to) {
-  // TODO
+export function fizzbuzz(input) {
+  if (input % 3 == 0) {
+    console.log('Fizz');
+  } else  if (input % 5 == 0) {
+    console.log('Buzz');
+  } else {
+    console.log(input);
+  }
+
 }
